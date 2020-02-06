4e56afa4662fca3e65142441983cccde9b164c62
Tobias Vidal <tobiasvidald@gmail.com>
2020-02-05 23:27:21 -0300

    Added lightbox functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    $('[data-img]').click(OpenInfo_Click);
 });
+
+function OpenInfo_Click() {
+    let imgSource = 'img/' + $(this).data('img');
+    //$('#lightbox-img').prop('src', imgSource);
+    $('#lightbox-trigger').prop('href', imgSource);
+    $('#lightbox-trigger').click();
+}
